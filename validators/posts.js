588dcdade860e027ec6d9b4158e0300c4b4af5ee
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-03-25 20:58:34 +0545

    added posts validator

---
+++ validators/posts.js
@@ -0,0 +1,21 @@
+const validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  const text = !isEmpty(data.text) ? data.text : "";
+
+  if (!validator.isLength(text, { min: 10, max: 300 })) {
+    errors.text = "Post must be between 10 and 300 characters.";
+  }
+
+  if (validator.isEmpty(text)) {
+    errors.text = "Text field is required.";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};

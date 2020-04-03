52ce1184966f7fc171ebcaac59eb5e7d8c5deebe
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-04-03 09:48:46 +0545

    config changes for heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const path = require("path");
 
 const app = express();
 
@@ -40,6 +41,15 @@ app.get("/", (request, response) => {
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
